6d187f0ba3cf1d44736c1955507ec3f6007242a1
Luan Victor <106767564+LuanME@users.noreply.github.com>
2022-06-15 17:30:24 -0300

    Adicionando Arquivo

---
+++ Aula 11/Aula 11/Calculadora.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Aula_11
+{
+    public class Calculadora
+    {
+        public double Valor { get; private set; }
+        public double Resultado { get; private set; }
+
+        public Calculadora(double valor, double resultado)
+        {
+            this.Valor = valor;
+            this.Resultado = resultado;
+        }
+
+
+        public int menu()
+        {
+            int op;
+            Console.WriteLine("Menu de opções da calculadora");
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("3 - Multiplicar");
+            Console.WriteLine("4 - Dividir");
+            Console.WriteLine("5 - Zerar Calculadora");
+            Console.WriteLine("0 - Terminar");
+            Console.WriteLine("Sua opção: ");
+            op = int.Parse(Console.ReadLine());
+
+            return op;
+        }
+
+       
+
+        public void zerarResultado()
+        {
+            this.Resultado = 0;
+        }
+
+
+
+    }
+}
---
+++ Aula 11/Aula 11/Mutiplicar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_11
+{
+    internal class Mutiplicar : Calculadora
+    {
+        public double multiplicarValor()
+        {
+            double valor;
+            double resultadoFuncao;
+            Console.WriteLine("Você digitou 3 - Multiplicar");
+            Console.WriteLine("Qual valor deseja Multiplicar?");
+            try
+            {
+                valor = double.Parse(Console.ReadLine());
+                resultadoFuncao = resultado * valor;
+                definirResultadoDaUltimaOperacaoMatematica(resultadoFuncao);
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Você deve digitar um número -> " + ex.Message);
+            }
+
+            return pegarResultadoDaUltimaOperacaoMatematica();
+        }
+    }
+}
